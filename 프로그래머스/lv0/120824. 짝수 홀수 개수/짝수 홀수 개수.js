d9acd92678e28a1877c2759c68b9df4e3c3da215
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2022-11-07 16:30:20 +0900

    Update 짝수 홀수 개수.js

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -5,3 +5,25 @@ function solution(num_list) {
   answer = [oddNum.length,evenNum.length]
     return answer;
 }
+
+function solution2(num_list) {
+
+  return([
+ num_list.filter((num)=>num % 2 === 0).length,
+ num_list.filter((num)=>num % 2 == 1).length
+  ])
+
+}
+
+
+// 다른 사람
+
+function solution1(num_list) {
+
+  let answer = [0,0];
+  for(let a of num_list) {
+    answer[a%2] += 1
+  }
+return (answer)
+
+}

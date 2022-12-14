f28b184494cc0f2e86d84ec579e0cfa997ed71fc
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2022-12-14 21:25:07 +0900

    [level 0] Title: 가위 바위 보, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,6 +1,9 @@
-function solution(rsp) {
-    const result = rsp.split('').map((element) => (
-        element === '2' ? '0' : element === '5' ? '2' : '5'))
-    
-    return result.join('');
+function solution([...rsp]) {
+    let arr = {
+        2: 0,
+        0: 5,
+        5: 2
+    };
+    var answer = rsp.map(v => arr[v]).join("");
+    return answer;
 }

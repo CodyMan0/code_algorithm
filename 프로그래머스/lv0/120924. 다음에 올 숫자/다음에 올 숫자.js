d4e9224eea5c8b21dd30999547f65862121b7e40
Brian <93697790+CodyMan0@users.noreply.github.com>
2023-01-11 19:49:00 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,25 +1,8 @@
-const solution = (common) => {
-
-  if (Math.sign(common[0])=== -1) {
-      if(common[2] % common[1] === 0) return common[0]**(common.length+1)
-    const dif = common[1] - common[0]
-    console.log(dif)
-    return common[0]+dif*(common.length) 
+function solution(common) {
+    if ((common[1]-common[0])==(common[2]-common[1])){
+        return common.pop() + common[1] - common[0];
     }
-
-    
-if (common[0] === 1){
-    if(common[2] % common[1] === 0) return common[1]**(common.length)
-    const dif = common[2] -common[1]
-    return common[0]+dif*(common.length)
-} 
-    
-    if(common[1] % common[0] === 0) return common[0]**(common.length+1)
-    const dif = common[2] -common[1]
-    return common[0]+dif*(common.length) 
-
-  
-
-
-    
+    else{
+        return common.pop()*common[1]/common[0];
+    }
 }

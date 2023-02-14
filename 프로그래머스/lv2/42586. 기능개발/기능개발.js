7dabe8986abc9aac339ce78189ed2d7332ac3d18
Brian <93697790+CodyMan0@users.noreply.github.com>
2023-02-14 10:17:17 +0900

    [level 2] Title: 기능개발, Time: 3.87 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,26 +1,29 @@
 function solution(progresses, speeds) {
-    let answer= [0]
-    //우선 몇일이 걸리는지를 파악하는 로직을 작성
-    const depDay= progresses.map((el,i) => {
-        const day = Math.ceil(((100-el )/ speeds[i]))
-        return day;
-    })
+    const queue=[...progresses].map((i,idx)=>[i,idx])
+    let days=1
     
-    let maxDay = depDay[0];
-
-    
-    for ( let i =0, j = 0 ; i< depDay.length; i++) {
-        if(depDay[i] <= maxDay) {
-        answer[j] += 1
-        } else {
-            maxDay = depDay[i]
-            answer[++j] = 1;
+    const result=[]
+    while(queue.length){
+        const [progress,idx]=queue.shift()
+        console.log(progress,idx)
+        while(100>progress+speeds[idx]*days){
+            console.log('a')
+            days++
         }
-        
+        result.push(days)
     }
-    
-    
+    console.log(result)
 
+    const deployDay=result.reduce((all,i)=>{
+        if(i in all){
+            all[i]+=1
+        }else{
+            all[i]=1
+        }
+        return all
+    },{})
+
+    console.log(deployDay)
     
-    return answer;
+   return Object.values(deployDay)
 }

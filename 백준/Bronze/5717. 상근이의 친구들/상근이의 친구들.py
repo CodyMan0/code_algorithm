a5533d7d20b82166f3ebda950cc783e5a73ed244
Brian <93697790+CodyMan0@users.noreply.github.com>
2023-04-20 10:13:24 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.py
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.py
@@ -1,7 +1,4 @@
-result = 0
-
 while True:
-    data = list(map(int,input().split()))
-    if data[0] == 0 : break 
-    result = data[0] + data[1]
-    print(result)
+    a,b = map(int,input().split())
+    if a == 0 : break   
+    print(a + b)

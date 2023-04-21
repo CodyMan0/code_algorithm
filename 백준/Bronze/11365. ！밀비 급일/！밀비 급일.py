665f622cd7ac22e59d836112d80fe8a219ec4b1a
Brian <93697790+CodyMan0@users.noreply.github.com>
2023-04-21 09:41:29 +0900

    [Bronze IV] Title: !밀비 급일, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/11365. ！밀비 급일/！밀비 급일.py
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.py
@@ -1,9 +1,4 @@
-for _ in range(5) :
-    str = input()
-    
-    if str == 'END'  :
-        break
-    else :
-        print(str[::-1])
-
-
+while True : 
+    a = input()
+    if a == 'END': break
+    else: print(a [::-1])

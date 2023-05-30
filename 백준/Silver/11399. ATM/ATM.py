543c29a420baec24f1685fa9a0e0952338af4915
이주영 <93697790+CodyMan0@users.noreply.github.com>
2023-05-30 11:56:17 +0900

    Update ATM.py

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -9,3 +9,16 @@ for i in range(1,queue+1):
 
 
 print(result)
+
+# 얕은 복사를 통해 문제를 풀 수도 있지만, 규칙을 찾아서 풀 수도 있다.
+
+import sys
+n = int(sys.stdin.readline())
+time = list(map(int, sys.stdin.readline().split()))
+time.sort()
+sum = 0
+for i in range(n) :
+    sum += time.pop() * (i + 1)
+
+print(sum)
+    

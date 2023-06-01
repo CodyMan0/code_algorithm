6f11c868b9099395828fcd154862b54cc5bec605
이주영 <93697790+CodyMan0@users.noreply.github.com>
2023-06-01 12:25:45 +0900

    Update 전쟁 － 땅따먹기.py

--- 백준/Silver/1270. 전쟁 － 땅따먹기/전쟁 － 땅따먹기.py
+++ 백준/Silver/1270. 전쟁 － 땅따먹기/전쟁 － 땅따먹기.py
@@ -22,3 +22,36 @@ for _ in range(int(input())) :
     if answer == 0 :
       print("SYJKGW") 
 
+
+# Map을 사용하지 않고 배열 안에서 가장 수가 많은 요소를 찾는 함수를 만들고 외부에서 해당 함수의 리턴값을 count 매소드를 활용하여 위와 동일한 로직을 구현할 수 있다. 
+
+## 어떤게 더 나은 로직일까? 
+### 둘다 시간 복잡도는 O(n)인 것 같다. 
+
+import sys
+
+input = sys.stdin.readline
+
+def find_majority_element(lst):
+    m = -1
+    i = 0
+    for j in range(len(lst)):
+        if i == 0:
+            m = lst[j]
+            i = 1
+        elif m == lst[j]:
+            i += 1
+        else:
+            i -= 1
+    return m
+
+
+T = int(input())
+for _ in range(T):
+    lst = list(map(int, input().split()))[1:]
+    v = find_majority_element(lst)
+    if lst.count(v) > len(lst) // 2:
+        print(v)
+    else:
+        print("SYJKGW")
+    

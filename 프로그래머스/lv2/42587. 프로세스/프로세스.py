ee708b642746a30203c50a65724b4e498430410b
이주영 <93697790+CodyMan0@users.noreply.github.com>
2023-06-08 11:11:37 +0900

    [level 2] Title: 프로세스, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.py
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.py
@@ -1,17 +1,14 @@
 def solution(priorities, location):
     answer = 0
-    wait_list = [(i,value) for i , value in enumerate(priorities)]
+    wait_list = [(i,value) for i,value in enumerate(priorities)]
     
-    while wait_list : 
+    while wait_list :
         first = wait_list.pop(0)
-        if any(first[1] < q[1] for q in wait_list) :
-            print('first',first)
+        
+        if any(first[1] < a[1] for a in wait_list ) :
             wait_list.append(first)
         else :
-            print('else',first)
             answer += 1
             if first[0] == location :
-                print(answer)
                 return answer
-
-    
+   

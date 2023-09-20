3d08e3f3931f9a5065cb6ed645fd4397b365de02
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-09-20 22:26:16 +0700

    solve: 9. Palindrome Number

---
+++ Leetcoe/day2.py
@@ -0,0 +1,11 @@
+## 9. Palindrome Number
+
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        return False if x < 0 else str(x)[::-1] == str(x)
+        

0bd0ab3aa16240dba11040291ed69cd5adf68ee1
mac <hys83751952@gmail.com>
2023-10-13 08:36:16 +0900

    10/13

--- Leetcode/13.Roman-to-integer/index.py
+++ Leetcode/13.Roman-to-integer/index.py
@@ -3,6 +3,7 @@ roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
 def romanToInt(s):
     answer = 0
+    print(answer)
     for i in range(len(s) - 1, -1, -1):
         num = roman[s[i]]
         if 4 * num < answer:

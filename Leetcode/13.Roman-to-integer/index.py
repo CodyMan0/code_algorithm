f5552977ec137169909707cee572f55a0a7366d8
mac <mac@macui-MacBookPro.local>
2023-10-13 08:24:29 +0900

    10.13

---
+++ Leetcode/13.Roman-to-integer/index.py
@@ -0,0 +1,17 @@
+roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+
+def romanToInt(s):
+    answer = 0
+    for i in range(len(s) - 1, -1, -1):
+        num = roman[s[i]]
+        if 4 * num < answer:
+            answer -= num
+        else:
+            answer += num
+    return answer
+
+
+romanToInt("III")
+romanToInt("LVIII")
+romanToInt("MCMXCIV")

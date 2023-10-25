c29485fdf463ef85df0b2f1086be86f3b3147cbf
Brian LEE <hys83751952@gmail.com>
2023-10-25 15:46:31 +0900

    10/25 연결 리스트 문제

--- Leetcode/week4-10.py
+++ Leetcode/week4-10.py
@@ -11,13 +11,33 @@ def isValid(s):
 
     return len(stack) == 0
 
+    ### isValid("()")
 
-isValid("()")
-isValid("()[]{}")
-isValid("(]")
+    ## IDEA : LIFO principle. therefore a stack is a good choice here
 
-## Stack?
-## 과거에 풀었는데 기억이 안남(외워서 그런듯)
-## (와 [ 와 { 면 append 아니면 pop
+    # Merge Two Sorted Lists
 
-## IDEA : LIFO principle. therefore a stack is a good choice here
+
+def mergeTwoLists(list1, list2):
+    head = ListNode()
+    current = head
+    while list1 and list2:
+        if list1.val < list2.val:
+            current.next = list1
+            list1 = list1.next
+        else:
+            current.next = list2
+            list2 = list2.next
+        current = current.next
+
+    current.next = list1 or list2
+    return head.next
+
+
+mergeTwoLists([-1, -2], [1, 3, 4])
+
+## 두 배열 깊게 복사한 후 sort 돌리면 안되나?
+
+## 연결 리스트와 관련된 질문이었다.
+
+## 둘다 오름차순으로 정렬되어있음.슬라이딩? 알고리즘 사용하면 안되나?

afa7c977f753fdbfa6e229acf95d17d7668f7a5e
Brian LEE <hys83751952@gmail.com>
2023-11-16 19:02:21 +0900

    Success Algorithm

--- Leetcode/week4-10.py
+++ Leetcode/week4-10.py
@@ -198,3 +198,23 @@ class Solution(object):
             one_before = total
 
         return total
+
+    # remove duplicated num in linkedList
+
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return None
+
+        curr = head
+
+        while curr.next:
+            if curr.val == curr.next.val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+
+        return head

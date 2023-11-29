39d5ad811fb6e025aea846b8a2e1db670fb06074
CodyMan0 <hys83751952@gmail.com>
2023-11-30 08:48:24 +0900

    Success : 11.30 tree traversal

--- Leetcode/week4-10.py
+++ Leetcode/week4-10.py
@@ -244,3 +244,14 @@ class Solution(object):
           nums1[m+j] = nums2[j]
       nums1.sort()
 
+# binary tree traversal 
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+       return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right) if root else []
